20f924fae3c9a9a335bf552c6874170830e896c3
cuixin <steven.cuixin@gmail.com>
2012-07-08 01:03:13 +0800

    重构了GameChannel，目前的netty3.x不支持真正的zero copy广播，4.0中暂时没用，先用了一个duplicate的方式实现，速度也能快很多。小遗憾吧~

--- src/main/java/com/github/xgameenginee/channel/GameChannel.java
+++ src/main/java/com/github/xgameenginee/channel/GameChannel.java
@@ -1,32 +1,56 @@
 package com.github.xgameenginee.channel;
 
-import org.jboss.netty.channel.group.DefaultChannelGroup;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
 
 import com.github.xgameenginee.buffer.GameDownBuffer;
 import com.github.xgameenginee.core.Connection;
 
 public class GameChannel {
-	private DefaultChannelGroup channel;
+	private static final Logger logger = org.slf4j.LoggerFactory.getLogger(GameChannel.class);
+		
+	private Set<Connection> list = new HashSet<Connection>();
+	
+	private String name;
 	
 	public GameChannel(String name) {
-		channel = new DefaultChannelGroup(name);
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
 	}
 	
 	public boolean addConnection(Connection c) {
-		return channel.add(c.getChannel());
+		logger.info(getName() + " joined " + c.getAttachment());
+		return list.add(c);
 	}
 	
 	public boolean removeConnection(Connection c) {
-		return channel.remove(c.getChannel());
+		logger.info(getName() + " removed " + c.getAttachment());
+		return list.remove(c);
 	}
 	
 	public void broadcast(Connection except, GameDownBuffer buffer) {
-		channel.remove(except.getChannel());
-		channel.write(buffer.getChannelBuffer());
-		channel.add(except.getChannel());
+		removeConnection(except);
+		for (Connection c: list) {
+			c.sendGameDownBuffer(buffer.duplicate());
+		}
+		addConnection(except);
 	}
 	
 	public void broadcast(GameDownBuffer buffer) {
-		channel.write(buffer.getChannelBuffer());
+		StringBuilder sBuilder = new StringBuilder();
+		sBuilder.append(getName() + " broadcast: " + buffer.getChannelBuffer().getShort(2)).append(" size = ").append(list.size());
+		sBuilder.append("{");
+		for (Connection c: list) {
+			sBuilder.append(c.getId()).append("=").append(c.getAttachment()).append(",");
+			c.sendGameDownBuffer(buffer.duplicate());
+		}
+		sBuilder.append("}");
+		logger.info(sBuilder.toString());
+//		channel.write(buffer.getChannelBuffer());
 	}
 }
